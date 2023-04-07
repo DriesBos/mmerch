1455bfcfa6ae83758d1c7bef6c6a2e4bae4c8d6a
DriesBos <info@driesbos.com>
2023-04-07 21:46:07 +0100

    deps: sass + nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'title',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [{ name: 'description', content: 'content' }],
+    },
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@import "@/assets/styles/vars/variables.sass"',
+        },
+      },
+    },
+  },
+
+  css: ['~/assets/styles/reset.css'],
+});
