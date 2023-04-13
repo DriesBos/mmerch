da35a228faf74e0f5b447bc3a9d39cce02dddf29
DriesBos <info@driesbos.com>
2023-04-13 18:54:17 +0100

    cleanup

--- components/TheShader.vue
+++ components/TheShader.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="shader">
     <!-- <img class="shader-Image" src="~/assets/images/michael.png" /> -->
-    <canvas
-      class="glslCanvas shader-Canvas"
-      data-fragment-url="https://www.shadertoy.com/view/4lBBRz"
-      width="600"
-      height="900"
-      data-textures="~/assets/images/michael.png"
-    ></canvas>
+    <!-- <canvas class="glslCanvas" data-fragment-url="shader.frag" data-textures="~/assets/images/michael.png" width="500" height="500"></canvas> -->
   </div>
 </template>
 
@@ -25,6 +19,14 @@
     transform: translate(-50%, -50%)
     height: 66vh
     width: auto
+    border: 2px solid green
   &-Canvas
     z-index: +1
+    display: block
+    border: 2px solid orange
+    position: absolute
+    left: 0
+    top: 0
+    width: 100vw
+    height: 100vh
 </style>
